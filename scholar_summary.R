a5a2720cdeac3599c214439520679e959ac2c253
Nicholas Clark <nicholas.j.clark1214@gmail.com>
2018-01-25 07:00:38 +1000

    added function description

--- scholar_summary.R
+++ scholar_summary.R
@@ -1,13 +1,17 @@
+#### Plot citation metrics data from GoogleScholar User profiles ####
+#'@param user Character string representing Google Scholar user profile ID
+#'@return A \code{ggplot2} object showing a graph of citation metrics, with a timestamp
+#' 
 scholar_summary = function(user){
 library(scholar)
 cit <- scholar::get_citation_history(user)
 tot <- scholar::get_num_articles(user) 
 prof <- scholar::get_profile(user)
 
-ggplot2:: ggplot(cit, ggplot2::aes(x = year, y = cites))+
-  ggplot2::geom_bar(stat='identity')+
-  ggplot2::theme_bw()+
-  ggplot2::xlab('Year')+
+ggplot2:: ggplot(cit, ggplot2::aes(x = year, y = cites)) +
+  ggplot2::geom_bar(stat = 'identity') +
+  ggplot2::theme_bw() +
+  ggplot2::xlab('Year') +
   ggplot2::ylab('Google Scholar\n citations')+
   ggplot2::annotate('text', label = format(Sys.time(), "%Y-%m-%d"),
            x = -Inf, y = Inf, vjust = 1.5, hjust = -0.08, size = 3) +
